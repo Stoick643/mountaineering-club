cf78ba6006a9e194b8c4393e275824fd21bade46
Stoick643 <darko.mulej@gmail.com>
2025-07-06 21:59:08 +0200

    railway port bugfix

--- app.py
+++ app.py
@@ -962,4 +962,5 @@ def handle_message(data):
     }, room='main_chat')
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, debug=True, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
